62c6d454162f7033f1b4d1a1e4a0c76c617fc9e9
ssoso27 <sohi8290@gmail.com>
2019-11-23 04:50:09 +0900

    [알림] 알림 서비스 토글
    - 레코드 파일 저장

--- app/src/main/java/com/example/audiorecorder/NoticeMainActivity.java
+++ app/src/main/java/com/example/audiorecorder/NoticeMainActivity.java
@@ -1,7 +1,15 @@
 package com.example.audiorecorder;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -10,15 +18,37 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class NoticeMainActivity extends AppCompatActivity {
-    TextView txt;
+    private final String TAG = "[NoticeMain]";
+    private TextView txt;
     private ToggleButton btnToggleNotice;
+
+    private int mAudioSource = MediaRecorder.AudioSource.MIC;
+    private int mSampleRate = 44100;
+    private int mChannelCount = AudioFormat.CHANNEL_IN_STEREO;
+    private int mAudioFormat = AudioFormat.ENCODING_PCM_16BIT;
+    private int mBufferSize = AudioTrack.getMinBufferSize(mSampleRate, mChannelCount, mAudioFormat);
+    public AudioRecord mAudioRecord = null;
+    public Thread mRecordThread = null;
+    public boolean isRecording = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice_main);
         txt=(TextView)findViewById(R.id.txt_thunder);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
 
         if(txt.getText().equals("천둥 소리"))
             txt.setTag("10");
@@ -64,14 +94,67 @@ public class NoticeMainActivity extends AppCompatActivity {
 
         }
 
+        mRecordThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                byte[] readData = new byte[mBufferSize];
+//                String mFilepath = "dorun.mp3";
+                String mFilepath = Environment.getExternalStorageDirectory().getAbsolutePath() +"/record.mp3";
+                File file = new File(mFilepath);
+                if (!file.exists()) {
+                    try {
+                        file.createNewFile();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                Log.d(TAG, "filepath is " + mFilepath);
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream(mFilepath, true);
+                } catch(FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                while(isRecording) {
+                    int ret = mAudioRecord.read(readData, 0, mBufferSize);  //  AudioRecord의 read 함수를 통해 pcm data 를 읽어옴
+                    Log.d(TAG, "read bytes is " + ret);
+
+                    try {
+                        fos.write(readData, 0, mBufferSize);    //  읽어온 readData 를 파일에 write 함
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+
+                mAudioRecord.stop();
+                mAudioRecord.release();
+                mAudioRecord = null;
+
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         btnToggleNotice = findViewById(R.id.toggle_notice);
         btnToggleNotice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (btnToggleNotice.isChecked()) {
                     Toast.makeText(getApplicationContext(), "Toggle ON", Toast.LENGTH_SHORT).show();
+                    isRecording = true;
+                    if(mAudioRecord == null) {
+                        mAudioRecord =  new AudioRecord(mAudioSource, mSampleRate, mChannelCount, mAudioFormat, mBufferSize);
+                        mAudioRecord.startRecording();
+                    }
+                    mRecordThread.start();
                 } else {
                     Toast.makeText(getApplicationContext(), "Toggle Off", Toast.LENGTH_SHORT).show();
+                    isRecording = false;
                 }
             }
         });
--- app/src/main/java/com/example/audiorecorder/SendSoundThread.java
+++ app/src/main/java/com/example/audiorecorder/SendSoundThread.java
@@ -1,13 +1,34 @@
 package com.example.audiorecorder;
 
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.logging.Handler;
 
 public class SendSoundThread extends Thread {
-    private Handler handler;
-    private boolean isRun = true;
+    private final String TAG = "[SendThread]";
 
-    public SendSoundThread(Handler handler) {
-        this.handler = handler;
+    private boolean isRun = true;
+    private String host = "localhost";
+    private int port = 1111;
+    private byte[] buffer;
+    public static DatagramSocket socket;
+
+    private AudioRecord recorder;
+    private int sampleRate = 48000;/**8000, 16000, 22050, 24000, 32000, 44100, 48000 Choose the best for the device and the bandwidth **/
+    private int channelConfig = AudioFormat.CHANNEL_CONFIGURATION_MONO;
+    private int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+    private int minBufSize = AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat);
+    private boolean status = true;
+
+    public SendSoundThread() {
     }
 
     public void stopForever() {
@@ -16,7 +37,38 @@ public class SendSoundThread extends Thread {
         }
     }
 
+    @Override
     public void run() {
+        this.isRun = true;
 
+//        try {
+//            DatagramSocket socket = new DatagramSocket();
+//            byte[] buffer = new byte[minBufSize];
+//            DatagramPacket packet;
+//            final InetAddress destination = InetAddress.getByName(host);
+            recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, sampleRate, channelConfig,audioFormat, minBufSize*10);
+            try {
+                recorder.startRecording();
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            }
+
+            while(status) {
+                //reading data from MIC into buffer
+                minBufSize = recorder.read(buffer, 0, buffer.length);
+
+                //putting buffer in the packet
+//                packet = new DatagramPacket(buffer, buffer.length, destination,port);
+
+//                socket.send(packet);
+                Log.d(TAG, minBufSize+"");
+            }
+
+//        } catch(UnknownHostException e) {
+//            Log.e("Ex", "UnknownHostException");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            Log.e("EX", "IOException");
+//        }
     }
 }
